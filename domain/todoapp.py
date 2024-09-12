cdbe80865e5e9f11585cc232daf74ec7a2b133d4
Jonathan LAURENT <johjo@ytreza.org>
2024-09-12 09:36:06 +0200

    feat : close task item

--- domain/todoapp.py
+++ domain/todoapp.py
@@ -3,11 +3,20 @@ from uuid import UUID, uuid5, NAMESPACE_URL
 
 from eventsourcing.application import Application
 from eventsourcing.domain import Aggregate, event
+from enum import Enum
+
+
+class ItemStatus(Enum):
+    OPEN = "Open"
+    CLOSED = "Closed"
+
 
 @dataclass
 class Item:
     index: int
     name: str
+    status: ItemStatus
+
 
 class TodoList(Aggregate):
     @event("Started")
@@ -17,7 +26,7 @@ class TodoList(Aggregate):
     @event("ItemAdded")
     def add_item(self, item):
         next_id = len(self.items) + 1
-        self.items.append(Item(index=next_id, name=item))
+        self.items.append(Item(index=next_id, status=ItemStatus.OPEN, name=item))
 
     def all_items(self):
         return self.items
@@ -25,6 +34,10 @@ class TodoList(Aggregate):
     @staticmethod
     def create_id(name: str) -> UUID:
         return uuid5(NAMESPACE_URL, f"/todo_list/{name}")
+
+    @event("ItemClosed")
+    def close(self, index):
+        self.items[index - 1].status = ItemStatus.CLOSED
 
 
 class TodoApp(Application):
@@ -40,8 +53,14 @@ class TodoApp(Application):
 
     def get_open_items(self, todolist_id):
         todolist: TodoList = self.repository.get(todolist_id)
-        return todolist.all_items()
+        return [item for item in todolist.all_items() if item.status == ItemStatus.OPEN]
+
 
     @staticmethod
     def open_todolist(name):
         return TodoList.create_id(name)
+
+    def close_item(self, todolist_id, item_index):
+        todolist: TodoList = self.repository.get(todolist_id)
+        todolist.close(index=item_index)
+        self.save(todolist)
--- test/test_todoapp.py
+++ test/test_todoapp.py
@@ -1,6 +1,6 @@
 import pytest
 
-from domain.todoapp import TodoApp, Item
+from domain.todoapp import TodoApp, Item, ItemStatus
 
 
 def test_register_todolist():
@@ -17,7 +17,7 @@ def test_add_first_item(item):
     todolist_id = app.start_todolist("my todolist")
     app.add_item(todolist_id, item)
 
-    assert app.get_open_items(todolist_id) == [Item(index=1, name=item)]
+    assert app.get_open_items(todolist_id) == [Item(index=1, status=ItemStatus.OPEN, name=item)]
     notifications = app.notification_log.select(start=1, limit=10)
     assert "TodoList.ItemAdded" in notifications[1].topic
 
@@ -29,7 +29,8 @@ def test_add_second_item(item):
     app.add_item(todolist_id, "buy water")
     app.add_item(todolist_id, item)
 
-    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy water"), Item(index=2, name=item)]
+    assert app.get_open_items(todolist_id) == [Item(index=1, status=ItemStatus.OPEN, name="buy water"),
+                                               Item(index=2, status=ItemStatus.OPEN, name=item)]
     notifications = app.notification_log.select(start=1, limit=10)
     assert "TodoList.ItemAdded" in notifications[2].topic
 
@@ -41,7 +42,7 @@ def test_open_todolist():
 
     todolist_id = app.open_todolist("my todolist")
 
-    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy water")]
+    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy water", status=ItemStatus.OPEN)]
 
 
 def test_open_todolist_when_two():
@@ -53,7 +54,24 @@ def test_open_todolist_when_two():
     app.add_item(todolist_id, "buy milk")
 
     todolist_id = app.open_todolist("first todolist")
-    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy water")]
+    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy water", status=ItemStatus.OPEN)]
 
     todolist_id = app.open_todolist("second todolist")
-    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy milk")]
+    assert app.get_open_items(todolist_id) == [Item(index=1, name="buy milk", status=ItemStatus.OPEN)]
+
+
+def test_close_item():
+    app = TodoApp()
+    todolist_id = app.start_todolist("my todolist")
+    app.add_item(todolist_id, "buy water")
+    app.add_item(todolist_id, "buy milk")
+    app.add_item(todolist_id, "buy eggs")
+
+    app.close_item(todolist_id, 2)
+
+    notifications = app.notification_log.select(start=1, limit=10)
+    assert "TodoList.ItemClosed" in notifications[4].topic
+
+    assert app.get_open_items(todolist_id) == [
+        Item(index=1, status=ItemStatus.OPEN, name="buy water"),
+        Item(index=3, status=ItemStatus.OPEN, name="buy eggs")]
