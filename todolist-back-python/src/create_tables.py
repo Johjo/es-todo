8b46495296d2162f9f662784efdfb5bd0bdf3f4e
Jonathan LAURENT <johjo@ytreza.org>
2025-01-22 16:17:46 +0100

    chore: move Procfile to good directory

--- todolist-back-python/src/create_tables.py
+++ todolist-back-python/src/create_tables.py
@@ -1,10 +1,17 @@
+import os
 import sqlite3
+
+from dotenv import load_dotenv
 
 from infra.sqlite.sdk import SqliteSdk
 
 
 def main():
-    sdk = SqliteSdk(sqlite3.connect("/db/todolist.db.sqlite"))
+    load_dotenv()
+    database_path = os.environ["DATABASE_PATH"]
+    print(f"*****Create tables for {database_path}*****")
+
+    sdk = SqliteSdk(sqlite3.connect(database_path))
     sdk.create_tables()
 
 if __name__ == "__main__":
