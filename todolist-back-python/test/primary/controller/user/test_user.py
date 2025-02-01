a622d2b3c4e4b8655f318833d0a3e9eab02e2d8f
Jonathan LAURENT <johjo@ytreza.org>
2025-02-01 07:42:10 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:todolist-back-python/test/primary/controller/user/test_user.py

--- todolist-back-python/test/primary/controller/user/test_user.py
+++ todolist-back-python/test/primary/controller/user/test_user.py
@@ -6,7 +6,7 @@ from src.dependencies import Dependencies
 from src.hexagon.shared.type import UserKey, TodolistKey, TodolistName
 from src.hexagon.user.create_todolist import CreateTodolist
 from src.hexagon.user.port import TodolistUuidGeneratorPort, UserRepositoryPort, UserSnapshot, TodolistSnapshot
-from src.primary.controller.use_case_dependencies import inject_use_cases
+from src.primary.controller.user import UserController
 from src.secondary.user.user_repository_in_memory import UserRepositoryInMemory
 
 
@@ -21,15 +21,6 @@ class TodolistUuidGeneratorForTest(TodolistUuidGeneratorPort):
         if self._next_uuid is None:
             raise Exception("feed next_uuid before generating todolist key")
         return TodolistKey(self._next_uuid)
-
-
-class UserController:
-    def __init__(self, dependencies: Dependencies):
-        self._dependencies = dependencies
-
-    def create_todolist(self, user_key: str, todolist_name: str):
-        use_case: CreateTodolist = self._dependencies.get_use_case(CreateTodolist)
-        use_case.execute(user_key=UserKey(user_key), todolist_name=TodolistName(todolist_name))
 
 
 class TestCreateTodolist:
